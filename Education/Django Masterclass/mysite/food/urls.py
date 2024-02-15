aed127c5e9c300365535fc5edaf84d125c68326c
Cameron <cameron_mckeel@yahoo.com>
2024-02-15 15:11:09 +0900

    personal

--- Education/Django Masterclass/mysite/food/urls.py
+++ Education/Django Masterclass/mysite/food/urls.py
@@ -5,5 +5,8 @@ app_name = 'food'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:item_id>/', views.detail, name = 'detail'),
+    path('add', views.create_item, name = 'create_item'),
+    path('update/<int:id>/',views.update_item, name='update_item'),
+    path('delete/<int:id>/', views.delete_item, name='delete_item'),
 
 ]
